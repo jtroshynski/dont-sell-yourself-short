faa5bced24c61f3c33ef473df618b6223f46b6a7
jtroshynski <jtroshynski@gmail.com>
2019-06-15 00:26:16 -0500

    Added hex image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import me from './images/me.jpg'
+import me from './images/hexJeremy2.png'
 import github from './images/GitHub-Mark/PNG/GitHub-Mark-Light-64px.png'
 import linkedin from './images/LinkedIn-Logos/In/Digital/White/2x/In-White-40@2x.png'
 
@@ -15,7 +15,7 @@ class App extends Component {
             Hi, my name is Jeremy Troshynski. I've been a software developer for two years. <br />
             Click the links below to see what I've been working on.
           </p>
-          <div classname='logo-container'>
+          <div className='logo-container'>
             <a href='https://github.com/jtroshynski'><img className='logo' src={github} alt="Github" /></a>
             <a href='https://www.linkedin.com/in/jeremytroshynski/'><img className='logo' src={linkedin} alt="LinkedIn" /></a>
           </div>
